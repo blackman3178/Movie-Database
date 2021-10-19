08bb86bc1d0752dd945ff0643f736ca9b1f32e47
Noah Blackman <nablackm@usc.edu>
2021-10-19 11:05:56 -0400

    more work done on server.js. added a post and get method, set up server

--- server.js
+++ server.js
@@ -22,3 +22,44 @@ const db = mysql.createConnection(
     console.log("connected to the movies database!!")
 );
 
+// add a movie to db
+app.post("/api/add-movie", ({ body }, res) => {
+    const sql = `INSERT INTO movie_names (movie_name) VALUES (?)`;
+    const params = [body.movie_name];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message});
+            return;
+        }
+        res.json({
+            message: "Success!",
+            data: body
+        });
+    });
+});
+
+//read all movies 
+app.get("/api/movies", (req, res) => {
+    const sql = "SELECT * FROM movie_names";
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json ({ error: err.message});
+            return;
+        }
+        res.json ({
+            message: "Success!",
+            data: rows
+        });
+    });
+});
+
+// if a NOT FOUND request is used
+app.use ((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
